c854381b0eb64cc4901cf2ec736c81c1222dd80f
Md Rifat Alam <mdrftlm@gmail.com>
2025-03-31 09:35:46 +0600

    L2 question page12345

--- lib/view/Islamic Quiz/Level 2/l2_question_page5.dart
+++ lib/view/Islamic Quiz/Level 2/l2_question_page5.dart
@@ -8,8 +8,229 @@ class L2QuestionPage5 extends StatefulWidget {
 }
 
 class _L2QuestionPage5State extends State<L2QuestionPage5> {
+  int counter_seconds=30;
+  @override
+  void initState() {
+    setTiner();
+    super.initState();
+  }
+  void setTiner()async{
+    Future.delayed(const Duration(seconds: 1)).then((value){
+      setState(() {
+        if(counter_seconds>0){
+          counter_seconds--;
+          setTiner();
+        }else{
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context)=>const L2QuestionPage5()));
+        }
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 120,
+              decoration: const BoxDecoration(
+                  color: Colors.indigo,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),)),
+              child: const Row(
+                children: [
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("Bangla Quiz",
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                  SizedBox(
+                    width: 108,
+                  ),
+                  Text(
+                    "5/10",
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundColor: Colors.lightGreen,
+                      child: Text(
+                        "Q/A",
+                        style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              height: 55,
+              width: 120,
+              decoration: BoxDecoration(
+                color: Colors.indigo,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    height: 30,
+                    width: 30,
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                                "assets/image/watch-removebg-preview.png"))),
+                  ),
+                  Text(
+                    counter_seconds.toString(),
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.yellow),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              height: 120,
+              width: 335,
+              decoration: BoxDecoration(
+                  color: Colors.indigo, borderRadius: BorderRadius.circular(30)),
+              child: const Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "মুসলমানরা কোন দিকে মুখ ",
+                        style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      Text("করে নামাজ পড়ে?", style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),)
+                    ],
+                  )),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 65,
+              width: 300,
+              decoration: const BoxDecoration(
+                  color: Colors.indigo,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50))),
+              child: const Center(
+                  child: Text("মসজিদে নববী",
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white))),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 65,
+              width: 300,
+              decoration: const BoxDecoration(
+                  color: Colors.indigo,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50))),
+              child: const Center(
+                  child: Text("কাবা",
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white))),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 65,
+              width: 300,
+              decoration: const BoxDecoration(
+                  color: Colors.indigo,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50))),
+              child: const Center(
+                  child: Text("বাইতুল মাকদিস",
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white))),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 65,
+              width: 300,
+              decoration: const BoxDecoration(
+                  color: Colors.indigo,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50))),
+              child: const Center(
+                  child: Text("মদিনা",
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white))),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            InkWell(
+              onTap: (){
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context)=>const L2QuestionPage5()));
+              },
+              child: Container(
+                height: 50,
+                width: 220,
+                decoration: BoxDecoration(
+                  color: Colors.deepOrangeAccent,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Center(
+                    child: Text(
+                      "Next",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
+                          color: Colors.white),
+                    )),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
